726fab71e716462a01944599ddc8b36fb4d6b6b3
gerasimov <kirillgerasimov06@yandex.ru>
2024-06-25 18:39:32 +0300

    move to pytest

--- tests/lzw_test.py
+++ tests/lzw_test.py
@@ -1,20 +1,22 @@
-import unittest
-from unittest.mock import patch, mock_open
-from ..algorithms.lzw import LZWCoding
+import pytest
+from app.algorithms.lzw import LZWCoding
 
 
-class TestLZW(unittest.TestCase):
-	def setUp(self):
-		self.lzw = LZWCoding(path='path', directory='directory')
+@pytest.fixture()
+def lzw():
+	lzw = LZWCoding('test.txt', 'test')
+	return lzw
 
-	def test_compress(self):
-		case = 'TOBEORNOTTOBEORTOBEORNOT'
-		res = self.lzw._compress(case)
-		expected_value = [84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263]
-		self.assertEqual(res, expected_value)
 
-	def test_decompress(self):
-		case = [84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263]
-		res = self.lzw._decompress(case)
-		expected_value = 'TOBEORNOTTOBEORTOBEORNOT'
-		self.assertEqual(res, expected_value)
+def test_compress(lzw):
+	case = 'TOBEORNOTTOBEORTOBEORNOT'
+	res = lzw._compress(case)
+	expected_value = [84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263]
+	assert res == expected_value
+
+
+def test_decompress(lzw):
+	case = [84, 79, 66, 69, 79, 82, 78, 79, 84, 256, 258, 260, 265, 259, 261, 263]
+	res = lzw._decompress(case)
+	expected_value = 'TOBEORNOTTOBEORTOBEORNOT'
+	assert res == expected_value
