4f3c1a452b56f61c0e13dcf93050418846f50ae5
scooby <kirillgerasimov06@yandex.ru>
2024-06-27 15:53:49 +0300

    complete new cli system

--- compresslib/main.py
+++ compresslib/main.py
@@ -23,21 +23,30 @@ def compress(
 		algorithm: CodingType = Option(CodingType.HUFFMAN,
 		                               '--algorithm', '-a',
 		                               help="The algorithm to be used for compression")):
-	match algorithm:
-		case CodingType.HUFFMAN:
-			algo = HuffmanCoding
-		case CodingType.LZW:
-			algo = LZWCoding
-		case _:
-			console.print("[red]Invalid algorithm[/red]")
-			return
+	try:
+		match algorithm:
+			case CodingType.HUFFMAN:
+				algo = HuffmanCoding
+			case CodingType.LZW:
+				algo = LZWCoding
+			case _:
+				console.print("[red]Invalid algorithm[/red]")
+				return
 
-	build = HuffBuild(directory=path.parent, file=path, algorithm=algo)
+		build = HuffBuild(directory=path.parent, file=path, algorithm=algo)
 
-	fn = build.execute_func()
-	with console.status("[bold green]Compressing...", spinner="growHorizontal"):
-		res = build.execute(func=fn)
-	console.print(res)
+		fn = build.execute_func()
+		with console.status("[bold green]Compressing...", spinner="growHorizontal"):
+			res = build.execute(func=fn)
+		console.print(res)
+	except:
+		tb = sys.exc_info()[2]
+		traceback = TraceBack(traceback=tb, info=sys.exc_info())
+		console.size = (65, 10)
+		console.print(traceback)
+		if confirm("Would you like to send issue?", default=False):
+			link = generate_issue_link(title=f'[AUTO ISSUE]', body=traceback.info)
+			launch(link)
 
 
 @app.command('ucmp')
@@ -45,20 +54,29 @@ def decompress(
 		path: Path = Argument(help="The path of the file to be decompressed", exists=True, file_okay=True,
 		                      formats=[".lzw", ".huff"]),
 		output: Path = Option(None, '--output', '-o', help="The path of the output file", dir_okay=True)):
-	match path.suffix:
-		case ".lzw":
-			algo = LZWCoding
-		case ".huff":
-			algo = HuffmanCoding
-		case _:
-			console.print("[red]Invalid algorithm[/red]")
-			return
+	try:
+		match path.suffix:
+			case ".lzw":
+				algo = LZWCoding
+			case ".huff":
+				algo = HuffmanCoding
+			case _:
+				console.print("[red]Invalid algorithm[/red]")
+				return
 
-	build = HuffBuild(directory=output or path.parent, file=path, algorithm=algo)
-	fn = build.execute_func()
-	with console.status("[bold green]Decompressing...", spinner="growHorizontal"):
-		res = build.execute(func=fn)
-	console.print(res)
+		build = HuffBuild(directory=output or path.parent, file=path, algorithm=algo)
+		fn = build.execute_func()
+		with console.status("[bold green]Decompressing...", spinner="growHorizontal"):
+			res = build.execute(func=fn)
+		console.print(res)
+	except:
+		tb = sys.exc_info()[2]
+		traceback = TraceBack(traceback=tb, info=sys.exc_info())
+		console.size = (65, 10)
+		console.print(traceback)
+		if confirm("Would you like to send issue?", default=False):
+			link = generate_issue_link(title=f'[AUTO ISSUE]', body=traceback.info)
+			launch(link)
 
 
 
