b5040b86557b8d531ad8f88ab573c26b8e1550b7
scooby <kirillgerasimov06@yandex.ru>
2024-06-27 14:18:20 +0300

    make new architecture of CLI

--- compresslib/main.py
+++ compresslib/main.py
@@ -9,6 +9,7 @@ from typer import Typer, Argument, Option, launch
 from rich.console import Console
 from typer import confirm
 from .utils.url import generate_issue_link
+from typing_extensions import Annotated
 import os
 import sys
 
@@ -16,47 +17,21 @@ app = Typer()
 console = Console()
 
 
-@app.command(name='huff')
-def execute_file(file: Path = Argument(..., exists=True),
-                 output: Path = Option('./', '-o', '--out-path', dir_okay=True,
-                                       help='Path to directory, works only with .huff files'),
-                 algorithm: CodingType = Option(CodingType.HUFFMAN, '-a', '--algorithm',
-                                                help='Compress or decompress file with huffman or lzw algorithm')
-                 ):
-	try:
-		match algorithm, file.suffix:
-			case _, '.lzw':
-				algorithm = LZWCoding
-			case _, '.huff':
-				algorithm = HuffmanCoding
-			case CodingType.HUFFMAN, _:
-				algorithm = HuffmanCoding
-			case CodingType.LZW, _:
-				algorithm = LZWCoding
+@app.command()
+def compress(
+		path: Annotated[Path, Argument(help="The path of the file to be compressed", exists=True)],
+		algorithm: Annotated[CodingType, Option(CodingType.HUFFMAN,
+		                                        '--algorithm', '-a',
+		                                        help="The algorithm to be used for compression")]):
+	pass
 
-			case _:
-				raise Exception('Wrong algorithm')
 
-		build = HuffBuild(directory=output, file=file, algorithm=algorithm)
-		fn = build.execute_func()
-		if fn == build.execute_compression:
-			with console.status("[bold green]Compressing...", spinner="growHorizontal"):
-				res = build.execute(func=fn)
-
-		else:
-			with console.status("[bold green]Decompressing...", spinner="growHorizontal"):
-				res = build.execute(func=fn)
-
-		console.print(res)
-
-	except Exception as e:
-		tb = sys.exc_info()[2]
-		traceback = TraceBack(traceback=tb, info=sys.exc_info())
-		console.size = (65, 10)
-		console.print(traceback)
-		if confirm("Would you like to send issue?", default=False):
-			link = generate_issue_link(title=f'[AUTO ISSUE]', body=traceback.info)
-			launch(link)
+@app.command()
+def decompress(
+		path: Annotated[str, Argument(help="The path of the file to be decompressed", exists=True, file_okay=True,
+		                              formats=[".lzw", ".huff"])],
+		output: Annotated[str, Option(help="The path of the output file", dir_okay=True)]):
+	pass
 
 
 if __name__ == "__main__":
